bf931c7f43aa5331fa12ab7fc2a573684c2d2017
deocleverson <deocleverson@gmail.com>
2022-09-12 17:42:50 -0300

    criação de projeto

---
+++ crud-spring--alunos/src/test/java/br/com/techrech/crudspringalunos/crud/CrudSpringAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techrech.crudspringalunos.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
